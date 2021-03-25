3b410dafc8a5182fad845f1a27cd3d71c99a101d
dandy1414 <dandyyusuf140897@gmail.com>
2021-03-25 21:16:53 +0700

    -Add HeroShayna component -Add BannerShayna component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HeaderShayna from '@/components/HeaderShayna.vue'
+  // @ is an alias to /src
+  import HeaderShayna from '@/components/HeaderShayna.vue';
+  import HeroShayna from '@/components/HeroShayna.vue';
+  import BannerShayna from '@/components/BannerShayna.vue';
 
-export default {
-  name: 'Home',
-  components: {
-    HeaderShayna
+  export default {
+    name: 'Home',
+    components: {
+      HeaderShayna,
+      HeroShayna,
+      BannerShayna
+    }
   }
-}
 </script>
